7009d2d89e0cbf1f2761b9ec2ee20f387db4890d
Felipe Cepluki <felipecepluki@gmail.com>
2022-05-08 14:23:58 -0300

    Add server

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from "nodemailer";
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "e45f10c9a3c507",
+    pass: "483cc4489b8520",
+  },
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: "Equipe Feedget <oi@feedget.com>",
+      to: "Felipe Cepluki <felipecepluki@gmail.com>",
+      subject,
+      html: body,
+    });
+  }
+}
